83da07b7bcbc5e7b5148dcda8660d5b7b00940f9
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2018-04-30 10:48:24 +0200

    Started files

--- collections.js
+++ collections.js
@@ -1,1 +1,23 @@
 Article = new Mongo.Collection("articles");
+
+Images = new FilesCollection({
+  collectionName: 'Images',
+  allowClientCode: false, // Disallow remove files from Client
+  onBeforeUpload(file) {
+    // Allow upload files under 10MB, and only in png/jpg/jpeg formats
+    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
+      return true;
+    }
+    return 'Please upload image, with size equal or less than 10MB';
+  }
+});
+
+if (Meteor.isClient) {
+  Meteor.subscribe('files.images.all');
+}
+
+if (Meteor.isServer) {
+  Meteor.publish('files.images.all', function () {
+    return Images.find().cursor;
+  });
+}
