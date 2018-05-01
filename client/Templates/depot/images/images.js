891df8e6f520581fa99d7fd5017d4e5219797e7b
christellePerrin <christelle.perrin7@gmail.com>
2018-05-01 18:07:37 +0200

    début interface dépôt d'imgs

--- client/Templates/depot/images/images.js
+++ client/Templates/depot/images/images.js
@@ -1,8 +1,39 @@
 
-Template.editImage.helpers({
+Template.editPhoto.helpers({
+  "selectedPhoto" : function(){
+    return selectedPhoto.get();
+  },
+  "photohasbeenuploaded" : function(photo_id){
+    if(selectedPhoto.get() === undefined){
+      if(uploadedPhoto.get() != undefined){
+        return true;
+      } else {
+        return false;
+      }
 
+    } else {
+      return selectedPhoto.get().photo_id == uploadedPhoto.get();
+    }
+    return selectedPhoto.get().photo_id != null;
+  },
 });
 
-Template.showImage.helpers({
+Template.showPhoto.helpers({
+  "showPhoto" : function(){
+    if(selectedPhoto.get() == null){
+      return Photos.findOne(uploadedPhotos.get());
+    }
+    return Photos.findOne(selectedPhotos.get().image_id);
+  }
+});
 
+Template.editPhoto.events({
+  "submit Photo" : function(e,t){
+    e.preventDefault();
+  if(selectedPhoto.get() === undefined){
+    photo_id = Photo.insert(photo);
+  } else {
+    photo_id = Photo.update(selectedPhoto.get()._id, {$set:photo});
+  }
+},
 });
--- client/Templates/restitution/restitution.js
+++ client/Templates/restitution/restitution.js
@@ -9,11 +9,12 @@ Template.restitution.helpers({
     var images = Images.find({_id: {$nin : ids}}).fetch();
     var data = _.union(articles, images);
     return data;
-  } 
+  }
 });
+
 Template.showMedia.helpers({
  render(){
   if(this.isArticle) return Template["mediaArticle"];
   else return Template["mediaImage"];
- } 
+ }
 });
