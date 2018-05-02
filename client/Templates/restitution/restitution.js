a2c636036aaca370482923945d56c5a99b44ba7b
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2018-05-02 15:44:43 +0200

    Position aléatoire dans restitution

--- client/Templates/restitution/restitution.js
+++ client/Templates/restitution/restitution.js
@@ -1,5 +1,9 @@
 
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * Math.floor(max)) + min;
+}
+
 Template.restitution.helpers({
   medias(){
     var articles = Article.find().fetch();
@@ -10,9 +14,18 @@ Template.restitution.helpers({
     console.log(ids);
     var images = Images.find({_id: {$nin : ids}}).fetch();
     var data = _.union(articles, images);
+    _.each(data, function(media){
+      media.position = {x:getRandomInt(100,500), y: getRandomInt(100,500)}; 
+    });
     return data;
   }
 });
+
+
+Template.showMedia.onRendered(function(){
+  ///
+});
+
 
 Template.showMedia.helpers({
  render(){
@@ -22,6 +35,17 @@ Template.showMedia.helpers({
  }
 });
 
+Template.showMedia.events({
+  'drag .medias': function (e) {
+    e.preventDefault();
+    console.log(e);
+    let element = e.target;
+    // element.style.position = 'absolute';
+    // element.style.top = e.clientY + 'px';
+    // element.style.left = e.clientX + 'px';
+
+  }
+});
 
 Template.registerHelper("debug", function(data){
   console.log(data);
