944acf1676e933513a85641a19db080e651fa51a
Jacques-Daniel PILLON <jdpillon@lesalternatives.org>
2018-05-02 12:01:31 +0200

    Passer les images d'un truc à l'autre...

--- server/main.js
+++ server/main.js
@@ -2,4 +2,10 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  let users = Meteor.users.find().fetch();
+  if(users.length == 0){
+    // First startup, creating admin user.
+    console.log("First startup, init app");
+    Accounts.createUser({username:"admin", password:"admin"});
+  }
 });
