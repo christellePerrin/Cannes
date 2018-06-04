2a068f684316d709383d8f9c48c40a8ac258223e
christellePerrin <christelle.perrin7@gmail.com>
2018-06-04 16:16:33 +0200

    màj

--- server/main.js
+++ server/main.js
@@ -2,13 +2,10 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+  /*
+  Accounts.config({forbidClientAccountCreation: true});
   let users = Meteor.users.find().fetch();
-  if(users.length == 0){
-    // First startup, creating admin user.
-    console.log("First startup, init app");
-    Accounts.createUser({username:"admin", password:"admin"});
-    Accounts.createUser({username:"Baptiste Thevelein", password:"Org@nisationDcables"});
-    Accounts.createUser({username:"Benoît Michaëly", password:"QuiNeS@utepas69"});
 
-  }
+
+  */
 });
